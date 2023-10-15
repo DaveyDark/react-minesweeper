c8f927ba0fc17afb9db9f5520d6c3ea5f4d9ea81
DaveyDark <devesh97531@gmail.com>
2023-10-15 14:08:38 +0530

    Tile reveal and flagging done

--- src/Mineweeper.tsx
+++ src/Mineweeper.tsx
@@ -9,8 +9,32 @@ interface BoardProps {
   mines: number,
 }
 
+function countMines(board: JSX.Element[][], i: number, j: number) {
+  let cnt = 0;
+  for(let a=-1; a<=1; a++) {
+    for(let b=-1; b<=1; b++) {
+      if(a==0 && b==0) continue;
+      if(i+a < 0 || j+b < 0 || i+a >= board.length || j+b >= board.length) continue;
+      if(board[i+a][j+b].props.mine) cnt++;
+    }
+  }
+  return cnt;
+}
+
+function revealTiles(board: JSX.Element[][], i: number, j: number) {
+  board[i][j] = cloneElement(board[i][j], {revealed: true})
+  if(board[i][j].props.value != 0 || board[i][j].props.mine) return;
+  for(let a=-1; a<=1; a++) {
+    for(let b=-1; b<=1; b++) {
+      if(a==0 && b==0) continue
+      if(i+a < 0 || j+b < 0 || i+a >= board.length || j+b >= board.length) continue
+      if(!board[i+a][j+b].props.mine  && !board[i+a][j+b].props.revealed) revealTiles(board,i+a,j+b)
+    }
+  }
+}
+
 // Generate the board based on given props
-function generateBoard(size: number, mines: number, handleTileFlip: Function): JSX.Element[][] {
+function generateBoard(size: number, mines: number, handleTileFlip: Function, handleFlag: Function): JSX.Element[][] {
   const board: JSX.Element[][] = [];
 
   // Create an empty board
@@ -22,10 +46,12 @@ function generateBoard(size: number, mines: number, handleTileFlip: Function): J
           key={`tile-${i}-${j}`}
           row={i}
           col={j}
-          mine={false} // Initialize all tiles as not mines
+          mine={false}
+          flagged={false}
           revealed={false} // Initialize all tiles as not revealed
           value={0} // You can set the value to any default value you want
           onflip={handleTileFlip}
+          onflag={handleFlag}
         />
       );
     }
@@ -43,18 +69,31 @@ function generateBoard(size: number, mines: number, handleTileFlip: Function): J
     }
   }
 
+  // Generate number values for tiles
+  for(let i=0; i<board.length; i++) {
+    for(let j=0; j<board.length; j++) {
+      board[i][j] = cloneElement(board[i][j], {value: countMines(board,i,j)})
+    }
+  }
 
   return board;
 }
 
 const Minesweeper: FC<BoardProps> = (props) => {
-  const [board, setBoard] = useState(generateBoard(props.size, props.mines, handleTileFlip));
+  const [board, setBoard] = useState(generateBoard(props.size, props.mines, handleTileFlip, handleFlag));
 
   function handleTileFlip(i: number, j: number) {
+    if(board[i][j].props.mine) {
+      alert("Game Over")
+    }
     const updatedBoard = [...board];
-    updatedBoard[i][j] = cloneElement(board[i][j], {
-      revealed: true,
-    });
+    revealTiles(updatedBoard,i,j);
+    setBoard(updatedBoard);
+  }
+
+  function handleFlag(i: number, j: number) {
+    const updatedBoard = [...board];
+    updatedBoard[i][j] = cloneElement(updatedBoard[i][j], {flagged: !board[i][j].props.flagged})
     setBoard(updatedBoard);
   }
 
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -6,22 +6,33 @@ import { FC } from "react";
 interface TileProps {
   mine: boolean,
   revealed: boolean,
+  flagged: boolean,
   row: number,
   col: number,
   onflip: Function,
+  onflag: Function,
   value?: number,
 }
 
 const Tile: FC<TileProps> = (props) => {
   function handleFlip() {
-    if(props.revealed) return;
+    if(props.revealed || props.flagged) return;
     props.onflip(props.row, props.col)
   }
 
+  function handleFlag(e: any) {
+    e.preventDefault()
+    props.onflag(props.row, props.col)
+  }
+
   return (
-    <div className={`board-tile ${props.revealed ? "tile-revealed": ""} ${props.mine ? "tile-mine":""}`} onClick={() => handleFlip()}>
+    <div 
+      className={`board-tile ${props.revealed && "tile-revealed"} ${props.mine && "tile-mine"} `} 
+      onClick={() => handleFlip()}
+      onContextMenu={(e) => handleFlag(e)}
+    >
       <div className="tile-inner">
-        <div className="tile-front"><div></div></div>
+        <div className={`tile-front ${props.flagged && "tile-flagged"}`}><div></div></div>
         <div className="tile-back"><div>{props.value}</div></div>
       </div>
     </div>
